6609b3c47bc0eb7d7c56b1500754bf30b6f1568c
tygedavis <tyge.davis@gmail.com>
2019-12-09 13:55:22 -0700

    Provided data in App.js with CartContext

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 //Contexts
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -21,20 +22,23 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
 			<ProductContext.Provider value={{ products, addItem }}>
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={ Products }
-				/>
+				<CartContext.Provider value={ cart }>
+					
+					<Navigation />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={ Products }
+					/>
+
+					<Route
+						path="/cart"
+						component={ ShoppingCart }
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
